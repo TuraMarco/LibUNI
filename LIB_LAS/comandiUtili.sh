25f389d653326e02c92b84ec4371e8ddd897275a
Marco <tura.marco.91@gmail.com>
2018-06-07 11:40:13 +0200

    Aggiunto altro

--- LIB_LAS/comandiUtili.sh
+++ LIB_LAS/comandiUtili.sh
@@ -5,7 +5,7 @@
 #  FILTRI BASE  #
 #################
 
-# LS --------------------------------------------------------------------------
+# LS --------------------------------------------------------------------------------------------------------------------------------------------
 # Elenco i file in un direttorio (la home nel esempio), se viene omesso il 
 #   path si prende di defoult il direttorio corrente: 
 ls /home 
@@ -26,7 +26,7 @@ ls /home
 #       |- --sort=size  ----> Ordina in base alla dimensione del file
 #       |_ --sort=time  ----> Ordina in base alla data di ultima modifica
 
-# CD -----------------------------------------------------------------------------
+# CD -----------------------------------------------------------------------------------------------------------------------------------------------
 # Non è un comando linux ma una direttiva nativa della bash, permette di navigare 
 #   all'interno dei direttori
 cd                          # Torna nella home dell ’ utente corrente
@@ -34,7 +34,7 @@ cd /path /della/directory   # Naviga nel path "/path/della/directory"
 cd ..                       # Naviga nel direttorio padre
 cd -                        # Torna nel direttorio precedente
 
-# LN -----------------------------------------------------------------------------
+# LN -----------------------------------------------------------------------------------------------------------------------------------------------
 # Comando che permette la creazione di HardLink o SimbolicLink, la differenza
 #   tra i due sta nel fatto che il SimbolicLink è semplicemente un puntatore 
 #   ad una entry mentre un HardLink è una struttura maggiormente complessa 
@@ -43,7 +43,7 @@ cd -                        # Torna nel direttorio precedente
 ln /path/to/file/path/to/hardlink   # Crea un hardlink
 ln -s /path/to/file/path/to/link    # Crea un link simbolico
 
-# PRINTF -------------------------------------------------------------------------
+# PRINTF -------------------------------------------------------------------------------------------------------------------------------------------
 # Come la funzione di libreria C questo comando permette di stampare testo formattato,
 #   la sintassi del comando è la seguente  
 printf <FORMATO> <ARGOMENTI>
@@ -56,11 +56,11 @@ printf <FORMATO> <ARGOMENTI>
 #   Un esempio di utilizzo può essere:
 printf "Nome: %s\nEta: %d" pippo 16
 
-# PWD ----------------------------------------------------------------------------
+# PWD ----------------------------------------------------------------------------------------------------------------------------------------------
 # Permette di stampare il diettorio corrente in formato assoluto
 pwd 
 
-# DU -----------------------------------------------------------------------------
+# DU -----------------------------------------------------------------------------------------------------------------------------------------------
 # Viene usato per ottenere informazioni riguardo all'uso dello spazio di 
 #   archiviazione per un file o una directory, i flag piu usati sono:
 #       |- -h ---> Mostra le dimensioni in modo facilmente leggibile
@@ -69,9 +69,9 @@ pwd
 #       |_ --exclude="*.txt" ---> Esclude tutti i file che rispettano il pattern (non supporta RegEx)
 #   Esempi notevoli di utilizzo sono:
 du /path/directory          # Stampa la dimensione di ogni nodo dell'albero
-du | sort -nr | head -10    # Mostra i 10 file piu voluminosi nel direttorio corrente (PIPE) 
+du | sort -nr | head -10    # Mostra i 10 file piu voluminosi nel direttorio corrente (PIPE)
 
-# FIND ---------------------------------------------------------------------------
+# FIND ---------------------------------------------------------------------------------------------------------------------------------------------
 # Comando che permette di trovare i file e le directory all'interno del sistema.
 #   Permette di specificare filtri per individuare i file secondo diversi parametri,
 #   Questi sono solo alcuni degli esempi notevoli:
@@ -92,7 +92,7 @@ find ! -name *. txt                    # Trovare tutti i file che NOT finiscono 
 find -size +100k -name *. txt          # Trovare tutti i file con dimensione superiore a 100k AND che finiscono per .txt
 find -user las -o - name *. txt        # Trovare tutti i file dell'utente las OR che finiscono per .txts
 
-# GREP ----------------------------------------------------------------------------------
+# GREP ----------------------------------------------------------------------------------------------------------------------------------------------------
 # Comando atto al filtraggio e rierca dei pattern all'interno difile e std_io, la versione estesa 'egrep' 
 #   possiede la compatibilità completa con le RegularExpression, mentre la 'grep' solo con una sottoparte. 
 #   I fleg più usati sono:
@@ -139,7 +139,7 @@ fi
 #   * ---> indica 0 o più occorrenze dell'atomo che lo precede
 #   + ---> indica 1 o più occorrenze dell'atomo che lo precede
 
-# CUT -------------------------------------------------------------------------------------
+# CUT -------------------------------------------------------------------------------------------------------------------------------------------------------
 # Comando che permette di estrarre porzioni di righe passate o attraverso 
 #   std_in o con un file.
 #   In base al tipo di flag che si usa si estrarranno o un certo numero di caratteri 
@@ -163,7 +163,7 @@ cut -d ' ' -f1,3        # Estraggo il primo ed il terzo campo, separati da uno s
 #   Nel caso non sia sufficente un singolo carattere come delimitatore è utile passare 
 #   ad utilizzare il comando awk.
 
-# AWK ---------------------------------------------------------------------------------------
+# AWK ---------------------------------------------------------------------------------------------------------------------------------------------------------
 # Il comando awk è sostitutivo al comando cut, permette di estrarre dei campi da una riga 
 #   usando stringhe arbitrarie come separatori, e molto altro.
 #   Alcuni esempi di utilizzo sono:
@@ -178,7 +178,7 @@ awk -F "campo1=" '{ print $2 }' | awk -F " campo2=" '{ print $1 }'
 #   In caso si vogliano filtrare dei campi in real-time, pu`o essere comoda 
 #   l’opzione -W interactive che evita il buffering.
 
-# SED ----------------------------------------------------------------------------------------
+# SED ----------------------------------------------------------------------------------------------------------------------------------------------------------
 # Come il comando awk il comando sed è un ricchissimo linguaggio di manipolazione stringhe, 
 #   che permette innanzitutto di eseguire sostituzioni all interno di un file o dello std_in 
 #   di un processo.
@@ -189,7 +189,7 @@ sed 's/pippo/pluto/gi' file.txt # Sostituisci tutte le occorrenze di pippo con p
 sed 's/^/INIZIO: /' file.txt     # Aggiungi all'inizio di ogni riga "INIZIO: "
 
 
-# SORT ---------------------------------------------------------------------------------------
+# SORT ---------------------------------------------------------------------------------------------------------------------------------------------------------
 # Questo comando è utile per ordinare righe in un file o sullo std_in, di seguito sono 
 #   riportati alcuni esempi notevoli
 sort file.txt               # Ordina alfabeticamente le righe del file
@@ -206,7 +206,7 @@ sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n
 #   Un fatto importante da tenere a mente, è che sort deve consumare l'intero output, se vine 
 #   alimentato da std_in, prima di produrre a sua volta un risultato 
 
-# UNIQ -----------------------------------------------------------------------------------------
+# UNIQ -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # Comando che permette di rimuovere le righe duplicate CONTIGUE, è quindi bene farlo 
 #   precedere da un comando sort, al fine di raggruppare gli eventuali duplicati non contigui.
 #   Alcuni esempi notevoli di utilizzo sono:
@@ -214,14 +214,14 @@ uniq            # Elimina i duplicati CONTIGUI
 uniq -c         # Indica il numero di occorrenze per ogni duplicato
 uniq -d         # Mostra SOLO i duplicati
 
-# HEAD ----------------------------------------------------------------------------------
+# HEAD ----------------------------------------------------------------------------------------------------------------------------------------------------
 # Il comando head permette di mostrare le prime 'n' righe di un file o dello
 #   std_in nel caso di un impiego all'interno di una PIPE.
 #   DI seguito alcuni esempi notevoli:
 head -2 file.txt        # Mostra solo le prime 2 righe del file
 head -c 5 file.txt      # Mostra solo i primi 5 caratteri del file
 
-# TAIL ----------------------------------------------------------------------------------
+# TAIL ----------------------------------------------------------------------------------------------------------------------------------------------------
 # Come il comando head il comando tail isola e restituisce un insieme di righe, con la differenza 
 #   che invece di essere le prime 'n' sono le ultime 'n' di un file o dello std_in, ad esempio:
 tail -3 file.txt        # Mostra le ultime 3 righe del file
@@ -239,7 +239,68 @@ tail -f -- pid 1234     # Continua a leggere fino a che il processo 1234 non ter
 #  PROCESSI & SEGNALI  #
 ########################
 
-# PS ----------------------------------------------------------------------------------------
+# PS ----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Il comando ps permette di ottenre informazioni dai processi attivi, 
+#   le principali opzioni sono:
+#       |- ax ---------> Visualizza tutti i processi, anche non propri
+#       |- u ----------> Mostra gli utenti proprietari del processo
+#       |- w ----------> Visualizza la riga di comando completa che ha originato il processo
+#       |- f ----------> Visualizza i rapporti di discendenza tra i processi
+#       |- U ----------> Mostra i processi dell utente (o piu utenti [separati da " "])
+#       |- -p <pid> ----> Visualizza Solo il processo con questi pid (o più pid [separati da ","]) 
+#       |_ -C <comando> ---------> Visualizza il processo creato con il comando
+
+# SEGNALI -----------------------------------------------------------------------------------------------------------------------------------------------------
+# I segnali sono uno strumento che permette a due processi di comunicare. Un processo pu`o infatti inviare ad un
+#   altro un segnale che lo avvisa di un determinato evento.
+#   Inoltre, un processo pu`o registrare presso il sistema operativo un handler apposito che, quando viene ricevuto
+#   il segnale associato, viene eseguito.
+#   Di seguito sono riportati i principali segnali:
+#       |- SIGINT (2) -----> Interrompe un processo (CTRL + c)
+#       |- SIGHUP (6) -----> Sveglia, spegne o riavvia un processo
+#       |- SIGKILL (9) ----> Termina il processo (non ignorabile)
+#       |- SIGTERM (15) -----> Termina il processo (ignorabile)
+#       |- SIGTSTP (20) -----> Blocca il terminale (CTRL + z)
+#       |- SIGSTOP (23) -----> Interrompe l'esecuzione (non ignorabile)
+#       |- SIGUSR1 -----> Segnale personalizzabile 1
+#       |_ SIGUSR2 -----> Segnale personalizzabile 2
+#
+#   Per iviare un segnale ad un processo si usa il comando kill con la sintassi:
+#
+#   kill -<SIGXXX> <pid>
+#
+#   Un esempio tra tutti può essere:
+kill -SIGKILL 150  # Invia un segnale SIGKILL al processo con PID =150
+#
+#   L'operazione duale di intercettazione di un segnale  si fa con il comando trap,
+#   questo comando permette inoltre di associare una funzione di handler alla cattura del segnale 
+#   segunedo la sintassi:
+#
+#   trap '<handler>' <SIGXXX>
+#
+#   Un esempio di impiego può essere:
+trap 'echo ciao' SIGUSR1
+#   Spesso è però preferibile usare una funzione come handler al posto di un singolo comando, ad esempio
+#
+# Definisco l’ handler personalizzato
+function logging () 
+{
+    case "$1" in
+        start   ) echo started ;;
+        stop    ) echo stop ;;
+    esac
+}
+#
+# Stoppa il logging quando viene ricevuto SIGUSR1
+trap 'logging stop' SIGUSR1
+logging start
+while :
+do
+    # Eseguo le varie elaborazioni
+    echo elaboro...
+done
+
+# JOB CONTROLL ----------------------------------------------------------------------------------------------------------------
 
 
 
